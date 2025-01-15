d170608addfe638f1b7f8cd9a529c02864b8f372
sofi1507 <frontalinisofia@gmail.com>
2025-01-15 22:14:25 +0100

    Update veicolo.py
    inserisco i commenti della classe veicolo

--- veicolo.py
+++ veicolo.py
@@ -2,35 +2,46 @@
 #Classe 4BS
 #Classe Veicolo
 
+#creazione delle liste che mi servono per la classe veicolo -> Marca, Colore, Alimentazione
 listaMarca = ["FIAT", "FERRARI", "AUDI", "BMW", "MASERATI", "VOLKSWAGEN", "ALFA ROMEO", "MERCEDES", "FANTIC", "BETA", "E-SCOOTER", "SUZUKI", "PEUGEOT", "HARLEY-DAVIDSON"]
 listaColore = ["nero", "bianco", "rosso", "blu", "verde", " fucsia", "arancione", "gialla", "viola", "grigio"]
 listaAlimentazione = ["diesel", "ibrido", "elettrico", "benzina"]
+listaVeicoli = []
+
+#creazione della stringa alfabeto e numeri
 alfabeto = "QWERTYUIOPASDFGHJKLZXCVBNM"
 numeri = "1234567890"
-listaVeicoli = []
+
+#Classe Veicolo
 class Veicolo:
+    #funzione iniziale con parametri self e targa
     def __init__(self, targa):
+        #impostazione della marca, modello, colore, cilindraata, alimentazione a mia scelta
         self.__marca = "FIAT"
         self.__modello = "Panda"
         self.__colore = "fucsia"
         self.__cilindrata = 1000
         self.__alimentazione = "diesel"
+        #creazione della lista targa 
         listaTarga = []
+        #per ogni elemento dellaa targa lo aggiungo alla lista
         for x in targa:
             listaTarga.append(x)
+        #per ogni elemento della lista della targa controllo che essa si del tipo "AB 123 CD" tramite le posizioni della lista
         for x in listaTarga:
             if listaTarga[0] in alfabeto and listaTarga[1] in alfabeto and listaTarga[7] in alfabeto and listaTarga[8] in alfabeto and listaTarga[2] == " " and listaTarga[6] == " " and listaTarga[3] in numeri and listaTarga[4] in numeri and listaTarga[5] in numeri:
                 self.__targa = targa.upper()
             else:
                 raise ValueError("Targa non valida")
-        
-        veicolo = (self.__marca, self.__modello, self.__cilindrata)
-        listaVeicoli.append(veicolo)
+#         
+#         veicolo = (self.__marca, self.__modello, self.__cilindrata)
+#         listaVeicoli.append(veicolo)
         
     #funzione necessaria per visualizzare la classe
     def __str__(self):
         return self.__class__.__name__ + str(self.__dict__)
     
+    #imposto le proprietà su marca, modello, colore, cilindrata, alimentazione, targa
     @property
     def marca(self):
         return marca
@@ -55,22 +66,25 @@ class Veicolo:
     def targa(self):
         return targa
     
+    #imposto le setter
+    
+    #è possibile camabiare la marca solamente se è all'interno della lista sennò ritorna errore
     @marca.setter
     def marca(self, value):
         if value.upper() not in listaMarca:
             raise ValueError ("La marca non è presente")
         
         self.__marca = value.upper()
-
         return 
     
     
+    #è possibile camabiare il modello
     @modello.setter
     def modello(self, value):
         self.__modello = value
-
         return
     
+    #è possibile cambiare il colore solamente se è all'interno della lista sennò ritorna errore
     @colore.setter
     def colore(self, value):
         if value.lower() not in listaColore:
@@ -79,14 +93,16 @@ class Veicolo:
         self.__colore = value.lower()
         return
     
+    #è possibile cambiare l'alimentazione solamente se è all'interno della lista sennò ritorna errore
     @alimentazione.setter
     def alimentazione(self, value):
         if value not in listaAlimentazione:
             raise ValueError ("L'alimentazione non è presente")
         
         self.__alimentazione = value
-        return 
-        
+        return
+    
+    #è possibile camabiare la cilindrata solamente se è un intero positivo multiplo di 100  
     @cilindrata.setter
     def cilindrata(self, value):
         if value % 100 != 0 or type(value) != int or value < 0:
@@ -105,7 +121,8 @@ class Veicolo:
 #     
 #     def __repr__(self):
 #         return f"Veicolo(marca={self.marca}, modello={self.modello}, targa={self.targa})"
-#     
+#
+#Faccio i test
 if "__main__" == __name__:
     v1 = Veicolo("AD 123 SE")
     v2 = Veicolo("AS 345 WS")
