8364911d08439ec55d2099004b6d09f684163886
sofi1507 <frontalinisofia@gmail.com>
2025-01-15 19:40:29 +0100

    Update auto.py
    creazione della classe auto ereditata da Veicolo con le nuove caratteristiche

--- auto.py
+++ auto.py
@@ -3,3 +3,49 @@
 #Classe Auto
 
 from veicolo import Veicolo
+
+class Auto(Veicolo):
+    def __init__(self, targa, numeroMaxPasseggeri, numeroPasseggeriTrasportati, maxCapacitàTrasporto):
+        super().__init__(marca, modello, colore, cilindrata, alimentazione, targa)
+        if numeroMaxPasseggeri < 0 and numeroMaxPassegeri > 9:
+            raise ValueError ("Non è possibile")
+        else:
+            self.__numeroMaxPasseggeri = numeroMaxPasseggeri
+        if numeroPasseggeriTrasportati > numeroMaxPasseggeri:
+            raise ValueError ("Non è possibile")
+        else:
+            self.__numeroPasseggeriTrasportati = numeroPasseggeriTrasportati
+        self.__maxCapacitàTrasporto = maxCapacitàTrasporto
+   
+    @property
+    def numeroMaxPasseggeri(self):
+        return numeroMaxPasseggeri
+    
+    @property
+    def numeroPasseggeriTrasportati(self):
+        return numeroPasseggeriTrasportati
+    
+    @property
+    def maxCapacitàTrasporto(self):
+        return maxCapacitàTrasporto
+    
+    @numeroMaxPasseggeri.setter
+    def numeroMaxPasseggeri(self, value):
+        if value < 0 and value > 9:
+            raise ValueError ("Non è possibile")
+        self.__numeroMaxPasseggeri = value
+        return
+    
+     @numeroPasseggeriTrasportati.setter
+     def numeroPasseggeriTrasportati(self, value):
+        if value > numeroMaxPasseggeri:
+            raise ValueError ("Non è possibile")
+        self.__numeroPasseggeriTrasportati = value
+        return
+    
+    @maxCapacitàTrasporto.setter
+    def maxCapacitàTrasporto(self, value):
+        self.__maxCapacitàTrasporto = value
+        return
+    
+    
